b4d2ec7d8a87d3c3e4b69371af3591b43c8515c4
Simonvic17 <simonvic84@gmail.com>
2023-07-19 12:53:30 +0200

    Added custom borders on each side to avoid dupplicated borders

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const grid = document.getElementById('grid')
+const DEFAULT_SIZE = 20
+
+let currentSize = DEFAULT_SIZE
 
 function setGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -8,9 +11,29 @@ function setGrid(size) {
         const gridEl = document.createElement('div')
         gridEl.classList.add('grid-el')
         grid.append(gridEl)
+
+        // This will add a border on top and left
+        gridEl.classList.add('border-top-left')
+    }
+
+    // In order to avoid duplicated borders while creating grids
+    // We have to add borders on some sides
+    // This will create a right border of the last grid items
+    const rightItems = document.querySelectorAll(`.grid-el:nth-child(${currentSize}n)`);
+    for(let i = 0; i < rightItems.length; i++) {
+        rightItems[i].setAttribute('data-right', 'true');
+        rightItems[i].classList.toggle('border-right');
+    }
+
+    // This will create a bottom border of the last bottom grid items
+    let gridItems = document.querySelectorAll('.grid-el');
+    const lastItems = Array.from(gridItems.slice(-`${size}`));
+    for(let i = 0; i < lastItems.length; i++) {
+        lastItems[i].setAttribute('data-bottom', 'true');
+        lastItems[i].classList.toggle('border-bottom')
     }
 }
 
 window.onload = () =>{
-    setGrid(16)
+    setGrid(DEFAULT_SIZE)
 }
